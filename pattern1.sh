6b2d7bb11bd11b724d68cbb3b7babe5c8f33dbb8
revathi2001 <54894782+revathi2001@users.noreply.github.com>
2021-02-22 11:40:29 +0530

    Add files via upload

---
+++ pattern1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter number of rows"
+read n
+for((i=0;i<n;i++))
+do
+        for((j=n-i-1;j>0;j--))
+        do
+                printf " " ;
+        done
+        for((x=0;x<=i;x++))
+        do
+                printf "#" ;
+        done
+        printf "\n" ;
+done
